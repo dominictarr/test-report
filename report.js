3c9b4858763b72a723a31e61bc53208417f59b3c
Dominic Tarr <dominic.tarr@gmail.com>
2011-07-31 20:40:20 +1000

    if user calls test(name) and then subreport(name) do not createtwo items with the same name, stick the subreport onto the first test

--- report.js
+++ report.js
@@ -21,7 +21,7 @@ function update(old, newer){
 
 function min (array,func){
   var m = 'success'
-  for(var i in array){
+  for(var i in array) {
     var n = func(array[i],i,array)
     if(n && n < m)
       m = n
@@ -29,8 +29,8 @@ function min (array,func){
   return m
 }
 
-function getStatus (test){
-  var m = min(test.failures,function (e){
+function getStatus (test) {
+  var m = min(test.failures,function (e) {
         if(e && e.name && e.name === "AssertionError")
           return stati.failure
         return stati.error
@@ -38,6 +38,7 @@ function getStatus (test){
     , n = test.tests ? min (test.tests, function (e) {
         return getStatus(e)
       }) : 'success'
+
   return n < m ? n : m
 }
 
@@ -47,7 +48,6 @@ function failureCount (test){
           return x + y
       }, 0 /*initial*/) : 0 )
 }
-
 
 function test(name,failures) {
   return  { 
@@ -72,7 +72,7 @@ function get(report,path) {
   var f = find(report.tests, path[0])
   if (!f) {
     f = test(path[0],[])
-    if (!report.tests) 
+    if (!report.tests)
       report.tests = []
     report.tests.push(f)
   }
@@ -81,18 +81,23 @@ function get(report,path) {
 
 Reporter.prototype = {
   subreport: function (name) {
-    var subreporter = new Reporter (name)
+    // what if someone has already called test(name) ?
+    // it should not create a second test with the same name, 
+    // it should upgrade the test into a report.
+    var subreporter = new Reporter (name, get(this.report, name))
+
     this.test(subreporter.report)
     return subreporter
   }, 
   test: function (name,error){
     var t
+
     if (Array.isArray(name) || 'string' === typeof name) {
       var t = get(this.report,name)
       if (arguments.length > 1)
         t.failures.push(error)
     }
-    else if ('object' === typeof name)
+    else if ('object' === typeof name && -1 === this.report.tests.indexOf(name))
       this.report.tests.push(name)
     return this
   },
@@ -108,14 +113,14 @@ Reporter.prototype = {
   }
 }
 
-function Reporter (filename){
+function Reporter (name, report){
   if(!(this instanceof Reporter)) return new Reporter(filename)
 
-  var r = this.report = test (filename)
+  var r = this.report = (report || test (name))
   
-  r.filename = filename
+  r.filename = name
   r.os = process.platform
   r.version = process.version
   r.meta = {}
-  r.tests = [] 
+  r.tests = r.tests || []
 }
